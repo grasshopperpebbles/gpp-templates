6e10e1335716472276323050c557a7f31e887bda
les.green <grasshopperpebbles.ldg@gmail.com>
2026-01-03 12:26:53 +0100

    fix: Update Strapi 5 database configuration in templates
    - Change client from 'mysql2' to 'mysql' (Strapi 5 requirement)
    - Simplify SSL configuration to boolean (not object)
    - Remove pool configuration (causes parsing issues in Strapi 5)
    - Add debug: false property
    - Fixes 'Cannot destructure property client' error
    
    Updated templates:
    - cms/strapi-dev/files/config/database.ts
    - cms/strapi-only/files/config/database.ts

--- cms/strapi-dev/files/config/database.ts
+++ cms/strapi-dev/files/config/database.ts
@@ -7,13 +7,7 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME', 'strapi_dev'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env.bool('DATABASE_SSL', false) ? {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-      } : false,
-    },
-    pool: {
-      min: env.int('DATABASE_POOL_MIN', 2),
-      max: env.int('DATABASE_POOL_MAX', 10),
+      ssl: env.bool('DATABASE_SSL', false),
     },
     debug: false,
   },
